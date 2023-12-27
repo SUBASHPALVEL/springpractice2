1ecc0339b946ef2c3e0d373ba5c4a70767351716
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-12-27 16:09:56 +0530

    FactoryDesignPattern

---
+++ src/main/java/com/example/springpractice2/FactoryDesignPattern.java
@@ -0,0 +1,51 @@
+package com.example.springpractice2;
+
+public class FactoryDesignPattern {
+    
+}
+
+
+/*
+
+// "Product interface"
+interface Animal {
+    void sound();
+}
+
+// "Concrete products"
+class Dog implements Animal {
+    @Override
+    public void sound() {
+        System.out.println("Woof!");
+    }
+}
+
+class Cat implements Animal {
+    @Override
+    public void sound() {
+        System.out.println("Meow!");
+    }
+}
+
+// "Factory interface"
+interface AnimalFactory {
+    Animal createAnimal();
+}
+
+// "Concrete factories"
+class DogFactory implements AnimalFactory {
+    @Override
+    public Animal createAnimal() {
+        return new Dog();
+    }
+}
+
+class CatFactory implements AnimalFactory {
+    @Override
+    public Animal createAnimal() {
+        return new Cat();
+    }
+}
+
+
+ */
